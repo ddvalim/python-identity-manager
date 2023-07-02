2a6efabf70b5502ec2df13d46160e95e180c887c
Diovana Valim <diovanavalimm@gmail.com>
2023-07-02 00:14:46 -0300

    clean code

--- did/generate_did.py
+++ did/generate_did.py
@@ -1,11 +1,4 @@
-import logging
-import sys
-
-# Import server.anchor from the path relative to where the scripts are being executed.
-sys.path.insert(1, './server')
 from holder import anchor
-
-logging.getLogger().setLevel(logging.ERROR)
 
 def generate_did(seed):
     TRUST_ANCHOR = anchor.AnchorHandle()
